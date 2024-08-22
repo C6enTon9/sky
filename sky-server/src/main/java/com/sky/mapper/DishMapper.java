80b8d39a60fc922a7d0ddfad373953973e6da98f
chen <1104551347@qq.com>
2024-08-22 21:12:26 +0800

    新增菜品

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -1,5 +1,8 @@
 package com.sky.mapper;
 
+import com.sky.annotation.AutoFill;
+import com.sky.entity.Dish;
+import com.sky.enumeration.OperationType;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -12,4 +15,11 @@ public interface DishMapper {
      */
     @Select("select count(id) from sky_take_out.dish where category_id = #{id}")
     Integer countByCategoryId(Long id);
+
+    /**
+     * 新增菜品
+     * @param dish
+     */
+    @AutoFill(OperationType.INSERT)
+    void insert(Dish dish);
 }
