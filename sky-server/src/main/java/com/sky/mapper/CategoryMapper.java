63940a6b05b10ed14236fad275589c4918e5a191
chen <1104551347@qq.com>
2024-08-22 11:14:53 +0800

    用aop实现公共字段的自动填充

--- sky-server/src/main/java/com/sky/mapper/CategoryMapper.java
+++ sky-server/src/main/java/com/sky/mapper/CategoryMapper.java
@@ -1,8 +1,10 @@
 package com.sky.mapper;
 
 import com.github.pagehelper.Page;
+import com.sky.annotation.AutoFill;
 import com.sky.dto.CategoryPageQueryDTO;
 import com.sky.entity.Category;
+import com.sky.enumeration.OperationType;
 import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
@@ -16,6 +18,7 @@ public interface CategoryMapper {
      * 新增分类
      * @param category
      */
+    @AutoFill(OperationType.INSERT)
     @Insert("insert into sky_take_out.category(type, name, sort, status, create_time, update_time, create_user, update_user) VALUES (#{type},#{name},#{sort},#{status},#{createTime},#{updateTime},#{createUser},#{updateUser}) ")
     void insert(Category category);
 
@@ -30,6 +33,7 @@ public interface CategoryMapper {
      *删除分类
      * @param id
      */
+    @AutoFill(OperationType.UPDATE)
     @Delete("delete from sky_take_out.category where id = #{id}")
     void deleteById(Long id);
 
@@ -37,6 +41,7 @@ public interface CategoryMapper {
      * 修改分类
      * @param category
      */
+    @AutoFill(OperationType.UPDATE)
     void update(Category category);
 
     /**
